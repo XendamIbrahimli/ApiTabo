da585eaba2a892f00e614a9c071a99e6b6e57ccc
BextiyarNovruzov <memmedlixedice318@gmail.com>
2024-12-19 19:56:01 +0400

    begin Api

---
+++ DAL/TaboDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tabo.Entities;
+
+namespace Tabo.DAL
+{
+    public class TaboDbContext:DbContext
+    {
+        public DbSet<Language> Languages { get; set; }
+        public TaboDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Language>(b =>
+            {
+                b.HasKey(x=>x.Code);
+                b.HasIndex(x => x.Name).IsUnique();
+                b.Property(x => x.Code).IsFixedLength(true).HasMaxLength(3);
+                b.Property(x=>x.Name).IsRequired().HasMaxLength(32);
+                b.Property(x=>x.Icon).HasMaxLength(128);
+                b.HasData(new Language
+                {
+                    Code = "az",
+                    Name="Azerbaijan",
+                    Icon= "https://cdn-icons-png.flaticon.com/512/330/330544.png"
+                });
+
+            });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Migrations/20241219151618_SeedDatasForLanguages.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tabo.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedDatasForLanguages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Languages",
+                columns: new[] { "Code", "Icon", "Name" },
+                values: new object[] { "az", "https://cdn-icons-png.flaticon.com/512/330/330544.png", "Azerbaijan" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Languages",
+                keyColumn: "Code",
+                keyValue: "az");
+        }
+    }
+}
