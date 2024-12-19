9f8bc984250cf06aba4bc43a0ad00de7d0dde895
XendamIbrahimli <xandamei-bp215@code.edu.az>
2024-12-20 01:17:48 +0400

    CRUD

---
+++ Services/Abstracts/ILanguageService.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Tabo.DTOs.Languages;
+
+namespace Tabo.Services.Abstracts
+{
+    public interface ILanguageService
+    {
+        Task CreateAsync(LanguageCreateDto dto);
+        Task<LanguageGetDto> GetByCodeAsync(string code);
+        Task<IEnumerable<LanguageGetDto>> GetAllAsync();
+        Task DeleteAsync(string code);
+        Task UpdateAsync(LanguageUpdateDto dto,string code);
+    }
+}
--- obj/Debug/net8.0/Tabo.AssemblyInfo.cs
+++ obj/Debug/net8.0/Tabo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tabo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da585eaba2a892f00e614a9c071a99e6b6e57ccc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tabo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tabo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
